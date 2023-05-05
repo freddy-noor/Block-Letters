77d01204f22a03e1b7e516ff069c41e1f6b1201a
Freddy <79088826+freddy-noor@users.noreply.github.com>
2023-05-05 17:51:54 +0800

    Create intials.py

---
+++ intials.py
@@ -0,0 +1,10 @@
+# Freddy Noor
+# Fun Fact: I am boring
+
+print("FFFFF  N   N")
+print("F      NN  N")
+print("F      N N N")
+print("FFF    N  NN")
+print("F      N   N")
+print("F      N   N")
+print("F      N   N")
